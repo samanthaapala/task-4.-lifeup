3e43bee0898cf6d32398aad6c21a3623451b79d0
Samantha Apala <s.apala@yandex.ru>
2021-05-29 16:37:27 +0300

    Добавьте файлы проекта.

---
+++ Задание 4. Увеличение жизни/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4.Увеличение_жизни
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+         
+            Console.WriteLine("Введите число этажей");
+            int Floor = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число ступеней на лестнице");
+            int Steps = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите сколько секунд занимает подъем на 1 ступень");
+            int Seconds = Convert.ToInt32(Console.ReadLine());
+
+            int lifeUp = (Floor * Steps * Seconds) / 60;
+
+            Console.WriteLine($"Продолжительность вашей жизни увеличилась на {lifeUp} мин.!");
+            Console.ReadKey();
+
+        }
+    }
+}
